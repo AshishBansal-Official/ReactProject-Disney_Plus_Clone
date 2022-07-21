ef345ebba061c230bbfcafbb0b95bedb00ae463e
AshishBansal-Official <59161798+AshishBansal-Official@users.noreply.github.com>
2022-07-21 22:32:51 +0530

    Made the structure of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
+import Recommends from './Recommends'
+import NewDisney from './NewDisney'
+import Originals from './Originals'
+import Trending from './Trending'
 
 const Home = () => {
   return (
     <Container>
       <ImgSlider />
       <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
     </Container>
   )
 }
