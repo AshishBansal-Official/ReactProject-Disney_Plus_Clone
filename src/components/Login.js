f4db212060d74abb55e9d78acb31e16114ccdc0e
AshishBansal-Official <59161798+AshishBansal-Official@users.noreply.github.com>
2022-08-18 00:21:27 +0530

    Added readme file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,86 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import {
+    selectUserEmail,
+    selectUserName,
+    selectUserPhoto,
+    setSignOutState,
+    setUserLoginDetails,
+} from "../features/user/userSlice";
+import { auth, provider } from "../services/firebase";
+import { signInWithPopup } from "firebase/auth";
 
 const Login = () => {
-  return (
-    <Container>
-      <Content>
-        <BgImage />
-        <CTA>
-          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
-          <SignUp>GET ALL THERE</SignUp>
-          <Description>
-            Get Premier Access to Raya and the Last Dragon for an additional fee 
-            with a Disney+ subscription. As of 03/26/21, 
-            the price of Disney+ and The Disney Bundle will increase by $1.
-          </Description>
-          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
-        </CTA>
-      </Content>
-    </Container>
-  )
-}
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    const userEmail = useSelector(selectUserEmail);
+    const userPhoto = useSelector(selectUserPhoto);
+
+    const navigate = useNavigate();
+
+    const setUser = (user) => {
+        dispatch(
+            setUserLoginDetails({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL,
+            })
+        );
+    };
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setUser(user);
+                navigate("/home", { replace: true });
+            }
+        });
+    }, [userName]);
+
+    const handleAuth = () => {
+        if (!userName) {
+            signInWithPopup(auth, provider)
+                .then((result) => {
+                    setUser(result.user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(errorMessage);
+                });
+        } else if (userName) {
+            auth.signOut()
+                .then(() => {
+                    dispatch(setSignOutState());
+                    navigate("/", { replace: true });
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
+        }
+    };
+
+    return (
+        <Container>
+            <Content>
+                <BgImage />
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                    <SignUp onClick={() => handleAuth()}>GET ALL THERE</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an
+                        additional fee with a Disney+ subscription. As of
+                        03/26/21, the price of Disney+ and The Disney Bundle
+                        will increase by $1.
+                    </Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+                </CTA>
+            </Content>
+        </Container>
+    );
+};
 
 const Container = styled.section`
     overflow: hidden;
@@ -42,76 +103,76 @@ const Content = styled.div`
 `;
 
 const BgImage = styled.div`
-  height: 100%;
-  background-position: top;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-image: url('/images/login-background.jpg');
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: -1;
-`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpg");
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+`;
 
 const CTA = styled.div`
-  /* background-color: red; */
-  margin-bottom: 2vw;
-  max-width: 650px;
-  flex-wrap: wrap;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin-top: 0;
-  align-items: center;
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-  transition-timing-function: ease-out;
-  transition: opacity 0.2px;
-  width: 100%;
+    /* background-color: red; */
+    margin-bottom: 2vw;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    transition-timing-function: ease-out;
+    transition: opacity 0.2px;
+    width: 100%;
 `;
 
 const CTALogoOne = styled.img`
-  margin-bottom: 12px;
-  max-width: 600px;
-  min-height: 1px;
-  display: block;
-  width: 100%;
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display: block;
+    width: 100%;
 `;
 
 const SignUp = styled.div`
-  font-weight: bold;
-  color: #f9f9f9;
-  background-color: #0063e5;
-  margin-bottom: 12px;
-  width: 100%;
-  letter-spacing: 1.5px;
-  font-size: 18px;
-  padding: 16.5px 0;
-  border: 1px solid transparent;
-  border-radius: 4px;
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
 
-  &:hover {
-    background-color: #0483ee;
-    cursor: pointer;
-  }
+    &:hover {
+        background-color: #0483ee;
+        cursor: pointer;
+    }
 `;
 
 const Description = styled.p`
-  color: hsla(0, 0%, 95.3%, 1);
-  font-size: 11px;
-  margin: 0 0 24px;
-  line-height: 1.5;
-  letter-spacing: 1.5px;
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
 `;
 
 const CTALogoTwo = styled.img`
-  width: 100%;
-  max-width: 600px;
-  margin-bottom: 20px;
-  display: inline-block;
-  vertical-align: bottom;
+    width: 100%;
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
 `;
 
-export default Login
+export default Login;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,9 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -13,7 +12,7 @@ const firebaseConfig = {
     storageBucket: "disney-plus-clone-481e7.appspot.com",
     messagingSenderId: "953760587421",
     appId: "1:953760587421:web:e904c7bb356a7eeb2d7e74",
-    measurementId: "G-QKKYQQ4PYG"
+    measurementId: "G-QKKYQQ4PYG",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -24,5 +23,5 @@ const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export {auth, provider, storage}
+export { auth, provider, storage };
 export default db;
